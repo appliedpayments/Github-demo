9a76455ecfef3be0b3f464fe4a33209ce44b4374
appliedpayments <jordy@appliedpaymentstech.com>
2021-09-13 11:26:35 +0200

    Update Hello World.py
    Added a UI for Hello World Program

--- Source Code/Hello World.py
+++ Source Code/Hello World.py
@@ -1,1 +1,6 @@
-print("Hello World")
+import tkinter as tk    
+from tkinter import ttk    
+win = tk.Tk()    
+win.title("Python GUI App")    
+ttk.Label(win, text="Hello Applied Payments").pack()    
+win.mainloop()
